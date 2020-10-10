a5cd006d3197a4eea75bdb57da69733ae7902c13
redline711 <redline711@gmail.com>
2020-10-10 19:47:40 +0300

    Table logic (#5)
    * Implement group and point cell selection
    
    * Implement keyboard navigation
    
    * Finish base table logic
    
    Co-authored-by: Vova Kalinichenko <vova.kalinichenko@dreamhost.com>

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,22 +1,91 @@
 import { ExcelComponent } from '@core/ExcelComponent'
+import { $ } from '@/core/dom'
 import { createTable } from '@/components/table/table.template'
 import { resizeHandler } from '@/components/table/table.resize'
-import { shouldResize } from '@/components/table/table.functions'
+import {
+	shouldResize,
+	nextSelector,
+	isCell,
+	matrix
+} from '@/components/table/table.functions'
+import { TableSelection } from '@/components/table/TableSelection'
 
 export class Table extends ExcelComponent {
 	static className = 'excel__table'
 
-	constructor($root) {
-		super($root, { listeners: ['mousedown'] })
+	constructor($root, options) {
+		super($root, {
+			iname: 'Table',
+			listeners: ['mousedown', 'keydown', 'input'],
+			...options
+		})
 	}
 
 	toHTML() {
 		return createTable()
 	}
 
+	prepare() {
+		this.selection = new TableSelection()
+	}
+
+	init() {
+		super.init()
+
+		this.selectCell(this.$root.find('[data-id="0:0"]'))
+
+		this.$on('formula:done', () => {
+			this.selection.current.focus()
+		})
+
+		this.$on('formula:input', text => {
+			this.selection.current.text(text)
+		})
+	}
+
+	selectCell($cell) {
+		this.selection.select($cell)
+		this.$emit('table:select', $cell)
+	}
+
 	onMousedown(event) {
 		if (shouldResize(event)) {
 			resizeHandler(this.$root, event)
+		} else if (isCell(event)) {
+			const $target = $(event.target)
+			if (event.shiftKey) {
+				const $cells = matrix($target, this.selection.current).map(id =>
+					this.$root.find(`[data-id="${id}"]`)
+				)
+
+				this.selection.selectGroup($cells)
+			} else {
+				this.selection.select($target)
+			}
 		}
+	}
+
+	onKeydown(event) {
+		const keys = [
+			'Enter',
+			'Tab',
+			'ArrowLeft',
+			'ArrowUp',
+			'ArrowRight',
+			'ArrowDown'
+		]
+
+		const { key } = event
+
+		if (keys.includes(key) && !event.shiftKey) {
+			event.preventDefault()
+			const id = this.selection.current.id(true)
+
+			this.selectCell(this.$root.find(nextSelector(key, id)))
+		}
+	}
+
+	onInput(event) {
+		this.$emit('table:input', $(event.target))
 	}
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,15 +3,14 @@ import { ExcelComponent } from '@core/ExcelComponent'
 export class Toolbar extends ExcelComponent {
 	static className = 'excel__toolbar'
 
-	constructor($root) {
-		super($root, { name: 'Toolbar', listeners: ['click'] })
+	constructor($root, options) {
+		super($root, {
+			name: 'Toolbar',
+			...options
+		})
 	}
 
 	toHTML() {
 		return
-	}
-
-	onClick(event) {
-		console.log(event)
 	}
 }
